9e295aa2957980ce7f2588d1eded661ac48916e8
PAVLOZ <PAVLOZ@ext.inditex.com>
2019-12-23 15:44:31 +0200

    Fixed data

--- rotate_image/src/main.rs
+++ rotate_image/src/main.rs
@@ -17,10 +17,9 @@ impl Solution {
 
 fn main() {
     let mut v = vec![
-        vec![5, 1, 9, 11],
-        vec![2, 4, 8, 10],
-        vec![13, 3, 6, 7],
-        vec![15, 14, 12, 16],
+        vec![1,2,3],
+        vec![4,5,6],
+        vec![7,8,9]
     ];
     Solution::rotate(&mut v);
     println!("{:?}", v);
